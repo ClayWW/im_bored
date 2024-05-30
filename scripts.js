1d98eec67bcc0d6f8a2284eef96c44ae22a0be37
Clay <clayw@udel.edu>
2024-05-29 21:24:32 -0400

    MORE ACTIVITIES

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 const activities = [
-    // Add your activities here
     { name: 'Go to the gym', timeOfDay: 'any', dayOfWeek: 'any', season: 'any' },
     { name: 'Go fossil hunting', timeOfDay: 'day', dayOfWeek: 'weekend', season: 'spring' },
     { name: 'Go look for cool rocks', timeOfDay: 'day', dayOfWeek: 'any', season: 'any' },
@@ -43,8 +42,47 @@ const activities = [
     { name: 'Go ice skating', timeOfDay: 'day', dayOfWeek: 'any', season: 'winter' },
     { name: 'Visit a botanical garden', timeOfDay: 'day', dayOfWeek: 'any', season: 'spring' },
     { name: 'Take a cooking class', timeOfDay: 'any', dayOfWeek: 'any', season: 'any' },
-    { name: 'Try a new coffee shop', timeOfDay: 'any', dayOfWeek: 'any', season: 'any' }
+    { name: 'Try a new coffee shop', timeOfDay: 'any', dayOfWeek: 'any', season: 'any' },
+    { name: 'Go horseback riding', timeOfDay: 'day', dayOfWeek: 'any', season: 'summer' },
+    { name: 'Attend a theater performance', timeOfDay: 'evening', dayOfWeek: 'any', season: 'any' },
+    { name: 'Go paddle boarding', timeOfDay: 'day', dayOfWeek: 'any', season: 'summer' },
+    { name: 'Visit a historical site', timeOfDay: 'day', dayOfWeek: 'any', season: 'any' },
+    { name: 'Go to a food festival', timeOfDay: 'day', dayOfWeek: 'weekend', season: 'summer' },
+    { name: 'Attend a workshop or class', timeOfDay: 'any', dayOfWeek: 'any', season: 'any' },
+    { name: 'Do a charity run/walk', timeOfDay: 'day', dayOfWeek: 'any', season: 'any' },
+    { name: 'Go fishing', timeOfDay: 'day', dayOfWeek: 'any', season: 'summer' },
+    { name: 'Play a sport', timeOfDay: 'any', dayOfWeek: 'any', season: 'any' },
+    { name: 'Go on a wine tour', timeOfDay: 'day', dayOfWeek: 'weekend', season: 'fall' },
+    { name: 'Visit an animal sanctuary', timeOfDay: 'day', dayOfWeek: 'any', season: 'any' },
+    { name: 'Attend a car show', timeOfDay: 'day', dayOfWeek: 'weekend', season: 'summer' },
+    { name: 'Go on a boat trip', timeOfDay: 'day', dayOfWeek: 'any', season: 'summer' },
+    { name: 'Go stargazing', timeOfDay: 'night', dayOfWeek: 'any', season: 'any' },
+    { name: 'Visit a science center', timeOfDay: 'day', dayOfWeek: 'any', season: 'any' },
+    { name: 'Go camping', timeOfDay: 'day', dayOfWeek: 'weekend', season: 'summer' },
+    { name: 'Take a pottery class', timeOfDay: 'any', dayOfWeek: 'any', season: 'any' },
+    { name: 'Go rollerblading', timeOfDay: 'day', dayOfWeek: 'any', season: 'summer' },
+    { name: 'Try a new bakery', timeOfDay: 'any', dayOfWeek: 'any', season: 'any' },
+    { name: 'Visit a beach town', timeOfDay: 'day', dayOfWeek: 'weekend', season: 'summer' },
+    { name: 'Go whale watching', timeOfDay: 'day', dayOfWeek: 'any', season: 'summer' },
+    { name: 'Attend a fitness class', timeOfDay: 'any', dayOfWeek: 'any', season: 'any' },
+    { name: 'Go for a bike ride', timeOfDay: 'day', dayOfWeek: 'any', season: 'any' },
+    { name: 'Visit a trampoline park', timeOfDay: 'day', dayOfWeek: 'any', season: 'any' },
+    { name: 'Explore a cave', timeOfDay: 'day', dayOfWeek: 'any', season: 'any' },
+    { name: 'Visit a hot spring', timeOfDay: 'day', dayOfWeek: 'any', season: 'winter' },
+    { name: 'Go zip lining', timeOfDay: 'day', dayOfWeek: 'any', season: 'summer' },
+    { name: 'Go to a flea market', timeOfDay: 'day', dayOfWeek: 'weekend', season: 'any' },
+    { name: 'Visit an escape room', timeOfDay: 'any', dayOfWeek: 'any', season: 'any' },
+    { name: 'Go paintballing', timeOfDay: 'day', dayOfWeek: 'any', season: 'any' },
+    { name: 'Try archery', timeOfDay: 'day', dayOfWeek: 'any', season: 'any' },
+    { name: 'Visit a butterfly garden', timeOfDay: 'day', dayOfWeek: 'any', season: 'spring' },
+    { name: 'Attend a craft fair', timeOfDay: 'day', dayOfWeek: 'weekend', season: 'any' },
+    { name: 'Go rock climbing', timeOfDay: 'any', dayOfWeek: 'any', season: 'any' },
+    { name: 'Try a new ice cream place', timeOfDay: 'any', dayOfWeek: 'any', season: 'summer' },
+    { name: 'Visit a lighthouse', timeOfDay: 'day', dayOfWeek: 'any', season: 'any' },
+    { name: 'Go to a botanical festival', timeOfDay: 'day', dayOfWeek: 'weekend', season: 'spring' },
+    { name: 'Go on a scavenger hunt', timeOfDay: 'day', dayOfWeek: 'any', season: 'any' }
 ];
+
 
 function getCurrentTimeOfDay() {
     const hours = new Date().getHours();
